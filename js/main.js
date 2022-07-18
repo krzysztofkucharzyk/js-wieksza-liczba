72bf14b8de3b8c569573552a92f4f3e1f9a350de
Krzysztof Kucharzyk <krzysztofkucharzyk@gmail.com>
2022-07-18 22:37:57 +0200

    poprawione ostateczne

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 let num1 = 2;
 let num2 = 5;
 
-function hi_low (num1, num2) {
+function hi_low (n1, n2) {
     
     let wieksza;
-    if (num1 > num2) {
-        wieksza = num1;       
+    if (n1 > n2) {
+        wieksza = n1;       
     } else {
-        wieksza = num2;
+        wieksza = n2;
     }
 
     return wieksza;
